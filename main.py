60232699e1cc2a617b27251395ae43c220328c99
jps723 <31663418+jps723@users.noreply.github.com>
2019-03-09 03:02:54 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def main(_):
     #                 [dcgan.h4_w, dcgan.h4_b, None])
 
     # Below is codes for visualization
-    OPTION = 1
+    OPTION = 2
     visualize(sess, dcgan, FLAGS, OPTION)
 
 if __name__ == '__main__':
